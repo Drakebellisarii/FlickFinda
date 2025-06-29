18cc279b2a8b4e10cca22756677812e4f4612935
Drakebellisarii <144170639+Drakebellisarii@users.noreply.github.com>
2025-06-29 15:09:10 -0400

    Update app.py

--- src/movie_app/app.py
+++ src/movie_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+from openai import OpenAI
 import os
 import random
 import requests 
